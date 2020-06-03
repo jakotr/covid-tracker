ec59c907e5424e2b91e03f0571b208946e0e13f3
Jakub Otreba <jakub.otreba1@gmail.com>
2020-06-03 18:12:47 +0200

    Updates

---
+++ precache-manifest.457e905872421149db698e4a6b864df5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c38eab8c96b83158f179f241daa4ea0d",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "57e2a6bf28de99298c8b",
+    "url": "/covid-tracker/static/css/main.174bdfaf.chunk.css"
+  },
+  {
+    "revision": "b4c44fa7e00512653519",
+    "url": "/covid-tracker/static/js/2.84f8748e.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/covid-tracker/static/js/2.84f8748e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57e2a6bf28de99298c8b",
+    "url": "/covid-tracker/static/js/main.814863c1.chunk.js"
+  },
+  {
+    "revision": "4b31e93247999fd237fa",
+    "url": "/covid-tracker/static/js/runtime-main.2fdeeb5d.js"
+  }
+]);
